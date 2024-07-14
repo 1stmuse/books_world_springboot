306a5497c6253f8b02bf0c42c99fe975a954b17d
1stmuse <olysegs@gmail.com>
2024-07-14 11:39:59 +0100

    init project with auth setuo

---
+++ src/test/java/com/muse/books_world/BooksWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muse.books_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
