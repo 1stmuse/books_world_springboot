0ed78f1cd26b1c0a9349686c399461191b53f547
1stmuse <olysegs@gmail.com>
2024-07-19 04:28:22 +0100

    setup auth and resgister endpoint

--- src/main/java/com/muse/books_world/BooksWorldApplication.java
+++ src/main/java/com/muse/books_world/BooksWorldApplication.java
@@ -1,13 +1,33 @@
 package com.muse.books_world;
 
+import com.muse.books_world.role.Role;
+import com.muse.books_world.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BooksWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksWorldApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(
+			RoleRepository roleRepository
+	){
+		return args -> {
+			if(roleRepository.findByName("USER").isEmpty()){
+				roleRepository.save(
+						Role.builder()
+								.name("USER").build()
+				);
+			}
+		};
 	}
 
 }
---
+++ src/main/java/com/muse/books_world/auth/RegistrationRequest.java
@@ -0,0 +1,31 @@
+package com.muse.books_world.auth;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+
+    @NotEmpty(message = "firstname should not be empty")
+    @NotBlank(message = "firstname should not be empty")
+    private String firstname;
+    @NotEmpty(message = "lastname should not be empty")
+    @NotBlank(message = "lastname should not be empty")
+    private String lastname;
+    @NotEmpty(message = "firstname should not be empty")
+    @NotBlank(message = "Email should not be empty")
+    @Email(message = "Wrong email format")
+    private String email;
+    @NotEmpty(message = "Password should not be empty")
+    @NotBlank(message = "Password should not be empty")
+    @Size(message = "Password should be at least 8 characters")
+    private String password;
+}
